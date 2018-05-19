1fa182aad15052e0d06002fc29440056a37c3c44
Zeljko Djuric <zdjuric@yandex.com>
2018-05-19 21:01:54 +0200

    [quacksFromFollowees] Added logic for displaying quacksFromFollowees, also handled requests for follow and unfollow.

--- dz2/controller/quackController.php
+++ dz2/controller/quackController.php
@@ -19,7 +19,56 @@ class QuackController extends BaseController
 	}
 
 	public function quacksFromFollowees(){
-		echo 'quacksFromFollowees';
+		$qs = new QuackService();
+
+		$this->registry->template->title = 'Following';
+		$this->registry->template->quackList = $qs->getQuacksFromFollowees();
+
+		$this->registry->template->show( 'quacks_followees' );
+	}
+
+	public function updateFollowees(){
+		session_start();
+
+		$qs = new QuackService();
+
+		if( !isset( $_POST['username'] ) || !preg_match( '/^[a-zA-Z ,-.]+$/', $_POST['username'] ) )
+		{
+			header( 'Location: ' . __SITE_URL . '/index.php?rt=quack/quacksFromFollowees');
+			exit();
+		}
+		else {
+			if($qs->getIdOfUser($_POST['username']) === null || $_SESSION['username'] === $_POST['username'])
+			{
+				header( 'Location: ' . __SITE_URL . '/index.php?rt=quack/quacksFromFollowees');
+				exit();
+			}
+			else
+			{
+				if($_POST['action'] === 'follow'){
+					if($qs->checkIfIFollow($_POST['username'])){
+						header( 'Location: ' . __SITE_URL . '/index.php?rt=quack/quacksFromFollowees');
+						exit();
+					}
+					else{
+						$qs->followUser($_POST['username']);
+						header( 'Location: ' . __SITE_URL . '/index.php?rt=quack/quacksFromFollowees');
+						exit();
+					}
+				}
+				else if($_POST['action'] === 'unfollow'){
+					if($qs->checkIfIFollow( $_POST['username'])) {
+						$qs->unfollowUser($_POST['username']);
+						header( 'Location: ' . __SITE_URL . '/index.php?rt=quack/quacksFromFollowees');
+						exit();
+					}
+					else{
+						header( 'Location: ' . __SITE_URL . '/index.php?rt=quack/quacksFromFollowees');
+						exit();
+					}
+				}
+			}
+		}
 	}
 
 	public function followers(){
