c3e66fb7ba16c734f1dc3aad1839ef002abe7c14
Zeljko Djuric <zdjuric@yandex.com>
2018-06-02 21:49:30 +0200

    [08 Uvod u JavaScript] Funkcije mogu imati proizvoljan broj parametara.

--- example.js
+++ example.js
@@ -9,3 +9,7 @@ sayHello = function(){
 var kvadrat = (x => {return x * x; });
 
 var zbroj = ((x, y) => {return x + y; });
+
+function proizvoljanBrojParametara(){
+    console.log(arguments[1]);
+}
